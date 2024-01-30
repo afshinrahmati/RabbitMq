6f1d2790f9ab38ce7cba7bf18a04639718c3be48
afshin <afshinrahmati93@gmail.com>
2024-01-30 14:50:37 +0330

    feat: :fire: what is Broker
    explain broker

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import * as express from "express";
+import * as cors from "cors";
+import { createConnection } from "typeorm";
 const app = express();
 
 
+createConnection().then(db => {
+  app.use(cors({
+    origin: ["http://localhost:3000", "http://localhost:8080", "http://localhost:4200"]
+  }));
 
+  app.use(express.json());
 
-
-
-
-app.listen(5210, () => {
-  console.log("I can Start On port: 5210");
-})
+  app.listen(8000, () => {
+    console.log("I can Start On port: 5210");
+  });
+});
